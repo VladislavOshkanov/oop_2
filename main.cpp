3897e2b91fffac8b682aa39e61db1045bae70460
Oshkanov Vladislav <ovs@live.ru>
2017-04-20 14:52:39 +0700

    final corrections

--- main.cpp
+++ main.cpp
@@ -3,22 +3,25 @@
 int main() {
  srand ( time ( 0 ) );
  Container <Shape*> figures;
- for (int i = 0; i < 30; i++) {
+ for (int i = 0; i < 50; i++) {
    int type = rand() % 6;
+   Shape * sh;
    try{
-     figures.pushLast( newFigure( type ) );
+     sh = newFigure ( type );
+     figures.pushLast( sh );
    }
-   catch (char const * str){
-     std::cout << str << std::endl;
+   catch ( char const * str ){
+      std::cout << str << std::endl;
    }
 
+
  }
- std::cout << "Quantity of figures:" << Shape::getCount() << '\n';
+ std::cout << "Quantity of figures before:" << Shape::getCount() << '\n';
  for (int i = 0; i < figures.getSize(); i++) {
    Shape * sh = figures.getNth ( i );
    sh->print(std::cout);
    delete sh;
  }
 
- std::cout << "Quantity of figures:" << Shape::getCount() << '\n';
+ std::cout << "Quantity of figures after:" << Shape::getCount() << '\n';
 }
